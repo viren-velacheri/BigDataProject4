ef03845706c6ceeb8c44460a44167946cf4ae1ff
shaniyur <shaniyur@node0.a3grp14.ut-cs378-s22-pg0.utah.cloudlab.us>
2022-03-27 17:56:44 -0600

    most of algorithm done

--- page_rank.py
+++ page_rank.py
@@ -1,5 +1,5 @@
 import pyspark
-from pyspark.sql.functions import lit, collect_list
+from pyspark.sql.functions import lit, collect_list, col
 import sys
 
 spark = pyspark.sql.SparkSession.builder.appName("PageRank").getOrCreate()
@@ -14,7 +14,18 @@ output_file = sys.argv[2]
 edgeDF = spark.read.csv(input_file, comment="#", sep=r'\t').toDF("src", "dst")
 # Create Vertex dataframe that shows all vertices in the graph along with respective rank
 # Use vertices and edges to create graph g
-vertex_ranks = edgeDF.select('src').union(edgeDF.select('dst')).distinct().withColumnRenamed('src','id').withColumn("Rank", lit(1))
+vertex_ranks = edgeDF.select('src').union(edgeDF.select('dst')).distinct().withColumnRenamed('src','id').withColumn("rank", lit(1))
 g = GraphFrame(vertex_ranks,edgeDF)
+iterations = 1
+
+for i in range(iterations):
+    contribs = g.outDegrees.filter('outDegree != 0').join(vertex_ranks,['id']).withColumn('contribution', col('rank')/col('outDegree'))
+    contribs_dst = edgeDF.join(contribs, edgeDF.src==contribs.id).select('dst','contribution').groupBy('dst').sum().withColumn('newRank',0.15 + 0.85 * col('sum(contribution)'))
+    vertex_ranks = vertex_ranks.join(contribs_dst, vertex_ranks.id==contribs_dst.dst, 'leftouter').na.fill(0.15).select('id','newRank').withColumnRenamed('newRank','rank')
+
+vertex_ranks.sort("rank", "id").coalesce(1).write.option("header", True).csv(output_file)
+
+spark.stop()
+    
 
 
