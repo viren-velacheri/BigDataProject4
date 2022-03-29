c57e9ed82296cc9e609d378196a4034e1315a852
shaniyur <48967858+shaniyur@users.noreply.github.com>
2022-03-29 18:35:44 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --packages graphframes:graphframes:0.8.2-spark3.2-s_2.12 page_rank.py hdfs://10.10.1.1:9000/web-BerkStan.txt output1
+../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --packages graphframes:graphframes:0.8.2-spark3.2-s_2.12 page_rank.py hdfs://10.10.1.1:9000/web-BerkStan.txt output
