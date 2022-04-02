621135909b7f4356b7f1b7fe1cb4b4e70d84bf0c
shaniyur <48967858+shaniyur@users.noreply.github.com>
2022-04-02 16:39:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+# This command will pull the necessary package for GraphFrame using spark-submit and run our program with the paths to the web graph dataset and subsequent output
 ../spark-3.2.1-bin-hadoop3.2/bin/spark-submit --packages graphframes:graphframes:0.8.2-spark3.2-s_2.12 page_rank.py hdfs://10.10.1.1:9000/web-BerkStan.txt output
