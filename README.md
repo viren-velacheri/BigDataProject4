2c3ba5b715b2626efab17337f161bb70de6b4457
Pranav Akinepalli <pranav.akinepalli@gmail.com>
2022-04-02 17:26:40 -0500

    Added comments to code

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 ## Task 1 Instructions
 
-Please ensure firstly that the web-BerkStan.txt dataset is moved to hdfs prior to execution. Make sure that it is located in the root directory as it will be
-referenced as such by our run script. Please also make sure that Spark is located one directory above the current directory with the program files. Lastly, please
-create a 'checkpoint' directory in hdfs under the root directory (i.e. such that it is /checkpoint in hdfs). Please clear this directory prior to each run of the
-program.
+Please ensure firstly that the web-BerkStan.txt dataset is moved to hdfs prior to execution. Make sure that it is located in the root directory as it will be referenced as such by our run script. Please also make sure that Spark is located one directory above the current directory with the program files. Lastly, please create a 'checkpoint' directory in hdfs under the root directory (i.e. such that it is /checkpoint in hdfs). Please clear this directory prior to each run of the program.
 
 To run the page rank program for task 1, execute the following command:
 
 <b> ./run.sh </b> 
 
-This will execute page rank using graph frames for 10 total iterations and will produce output locally as a csv in a directory called "output" (NOT IN HDFS).
-The csv contained within this directory will have information about vertex id's and final ranks.
+This will execute page rank using graph frames for 10 total iterations and will produce output locally as a csv in a directory called "output" (NOT IN HDFS). The csv contained within this directory will have information about vertex id's and final ranks.
--- page_rank.py
+++ page_rank.py
@@ -1,25 +1,31 @@
 import pyspark
-from pyspark.sql.functions import lit, collect_list, col, coalesce, col, lit, sum, when
+from pyspark.sql.functions import lit, coalesce, sum
 import sys
 
-spark = pyspark.sql.SparkSession.builder.appName("PageRank").getOrCreate()
-spark.sparkContext.setCheckpointDir('hdfs://10.10.1.1:9000/checkpoint')
-from graphframes.examples import Graphs
-from graphframes import *
+from graphframes import GraphFrame
 from graphframes.lib import Pregel
 
-# read in arguments
+# create SparkSession
+spark = pyspark.sql.SparkSession.builder.appName("PageRank").getOrCreate()
+# provide location of checkpoint directory in HDFS
+spark.sparkContext.setCheckpointDir('hdfs://10.10.1.1:9000/checkpoint')
+
+# read in arguments - directory where input dataset is located and where to write output
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 # All edges in the graph based on relationships in dataset
 edgeDF = spark.read.csv(input_file, comment="#", sep=r'\t').toDF("src", "dst")
-# Create Vertex dataframe that shows all vertices in the graph along with respective rank
-# Use vertices and edges to create graph g
+# Create Vertex dataframe that shows all vertices in the graph along with respective out-degree
 vertex_DF = edgeDF.select('src').union(edgeDF.select('dst')).distinct().withColumnRenamed('src','id')
 vertices = GraphFrame(vertex_DF,edgeDF).outDegrees
+# Use vertices and edges to create graph g
 g = GraphFrame(vertices,edgeDF)
 
+# receive output dataframe after algorithm has run for 10 iterations
+# specify "rank" as column to update with initial values of 1 and update according to formula
+# use coalesce to substitute 0 for null values (vertices with no contributions)
+# calculate contributions from rank / outDegree and sum to get total contributions
 ranks = g.pregel \
      .setMaxIter(10) \
      .withVertexColumn("rank", lit(1.0), \
@@ -28,10 +34,7 @@ ranks = g.pregel \
      .aggMsgs(sum(Pregel.msg())) \
      .run()
 
-
+# select output of just user IDs and ranks and sort accordingly before writing to output location
 ranks.select('id','rank').sort("rank", "id").coalesce(1).write.option("header", True).csv(output_file)
 
 spark.stop()
-    
-
-
